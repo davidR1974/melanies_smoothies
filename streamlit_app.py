5313292da8ef84b382e87731ad5be393afd5c38b
davidR1974 <77006061+davidR1974@users.noreply.github.com>
2025-01-21 15:48:06 +0100

    Update streamlit_app.py
    correct typo "

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,21 +19,17 @@ cnx=st.connection("snowflake")
 session =cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients", my_dataframe,
     max_selections=5
 )
-
-
 
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' nutrition facts')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(name_on_order, ingredients)

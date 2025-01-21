597034a88a4c83f995d390ac737f646707b63ec9
davidR1974 <77006061+davidR1974@users.noreply.github.com>
2025-01-21 15:43:50 +0100

    Update streamlit_app.py
    correct typo True

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,13 +21,12 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 
-
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients", my_dataframe,
     max_selections=5
 )
 
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=true)
+sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
 
 if ingredients_list:
     ingredients_string = ''
